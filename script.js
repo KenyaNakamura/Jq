b671e5291215a9ef464fe1ba317b733fb1021195
nanae <7a1021@gmail.com>
2022-10-03 10:56:01 +0900

    リヴァイ、アルミン部分とスライダー

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function () {
     $(window).on('scroll', function () {
         // idがq-11の要素の画面最上部からの距離を取得
         let q11 = $('.fadein').offset().top;
-    
+
         // 画面の高さ
         let wh = $(window).height();
         q11 = q11 + 350;
@@ -12,13 +12,54 @@ $(function () {
         } else {
             $('.fadein').addClass('hide');
         }
-    
-    
+
+
     })
     // ハンバーガーメニュー
-    $('.hamburger').on('click', function(){
+    $('.hamburger').on('click', function () {
         $('.nav-list').toggleClass('is-active');
-        });
+    });
 
-    
+    // リヴァイ
+    $(window).on('scroll', function () {
+        let levi = $('.levi').offset().top;
+        let wh = $(window).height();
+        // $(window).scrollTop()は現在のスクロール位置
+        if (levi <= $(window).scrollTop() + wh) {
+            $('.levi').removeClass('levihide');
+        } else {
+            $('.levi').addClass('levihide');
+        }
+
+    })
+    //   アルミン
+   
+    // alumin = alumin + 250
+    $(window).on('scroll', function () {
+        let alumin = $('.alumin').offset().top;
+        let wh2 = $(window).height();
+
+        if (alumin <= $(window).scrollTop() + wh2) {
+            $('.alumin').removeClass('aluminhide');
+        } else {
+            $('.alumin').addClass('aluminhide');
+        }
+
+    })
+
+
+    $('.slide-container').slick({
+        autoplay: true, // 自動再生
+        arrows: true, // 前・次のボタンを表示する
+        dots: true, // ドットナビゲーションを表示する
+        appendDots: $('.dots-2'), // ドットナビゲーションの生成位置を変更
+        speed: 500, // スライドさせるスピード（ミリ秒）
+        autoplaySpeed: 3000
+       
+       
+      });
+
+
+
+
 })
